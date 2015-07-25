d3330bc06e334c0360046f62576fd0b8287d9c8b
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-07-25 14:29:28 -0600

    Stringify simple cases:  numbers, strings, booleans, and arrays. 12/20  pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,18 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // base cases
+  if(obj===null){
+    return "null";
+  }
+  if(typeof obj === "string"){
+    return  "\u0022"+obj+"\u0022";
+  }
+  if(typeof obj === "number" || typeof obj === "boolean"){
+    return obj.toString();
+  }
+  if(Array.isArray(obj)){  //If array
+    var strung=_.map(obj,stringifyJSON);
+    return "[" + strung + "]";
+  }
 };
