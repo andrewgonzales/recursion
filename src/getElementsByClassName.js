ccaf92825033a72f5c2f91df4495424b644339c0
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-07-25 20:28:28 -0600

    Added helper functions, one layer support

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var elements = [];
+  var hasTheClass = function(node){
+  	return _.contains(node.classList, className);
+  };
+  var hasChildren = function(node){
+  	return node.childNodes.length>0;
+  };
+  var addElement = function(node){
+  	if(hasTheClass(node)){
+  	  elements.push(node);
+  	}
+  };
+  addElement(document.body);
+  return elements;
 };
