88fd9253d0cdf9253f81dd7f17b9df41d53d274b
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-17 15:58:26 -0700

    Made stringifyJSON pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-
+  // if base case
+  if(typeof obj === "number" || typeof obj === "boolean" || obj === null)
+    return "" + obj;
+  // if string add quotes
+  if (typeof obj === "string"){
+    return '"' + obj + '"';
+  }
+  // if obj is an array
+  if (Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+    // return '[' + something + ']';
+  } else if (typeof obj === "object") {
+    var storage = [];
+    for (var key in obj){
+      if (typeof obj[key] !== "function"  && obj[key] !== undefined) {
+        storage.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + storage.join(',') + '}';
+  }
+  // if (typeof obj === "object" && !Array.isArray(obj) && obj !== undefined && obj !== null) {
+  //  if(Object.getOwnPropertyNames(obj).length===0) {
+  //   return '{' + obj + '}'
+  //   }
+  // }
 };
