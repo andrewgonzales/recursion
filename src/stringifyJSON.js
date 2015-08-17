c6ebd69115de87b52353da8d6d0b0269436b625b
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-17 11:44:23 -0700

    Reset stringifyJSON for sprint

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,42 +4,5 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // base cases
-  if(obj===null){
-    return "null";
-  }
-  if(typeof obj === "string"){
-    return  "\u0022"+obj+"\u0022";
-  }
-  if(typeof obj === "number" || typeof obj === "boolean"){
-    return obj.toString();
-  }
-  if(Array.isArray(obj)){                       //If array
-    var strung=_.map(obj,stringifyJSON);
-    return "[" + strung + "]";
-  }else {                                        //If object
-    var strung;      
-  	if(Object.getOwnPropertyNames(obj).length===0){ //if empty object
-      strung = "";
-  	  return "{" + strung + "}";
-  	}
-  	if(Object.getOwnPropertyNames(obj).length>0){
-  		var strungKeys = [];
-  		var strungVals = [];
-  	  _.each(obj, function(val, key){
-  	  	if(typeof val !== "function" && typeof val !== "undefined"){
-  	  	  strungKeys.push(stringifyJSON(key));
-  	  	  strungVals.push(stringifyJSON(val));
-  	  	}
-  	  });
-  	  var strung = "";
-  	  for(var i = 0; i<strungKeys.length;i++){
-  	  	strung = strung +[strungKeys[i]] + ":" + strungVals[i];
-  	  	if(i<strungKeys.length-1){
-  	  		strung = strung + ",";
-  	  	}
-  	  }
-  	return "{" + strung + "}";
-    }
- }
+
 };
